89860bb204177c8482b745c787867dcdcb99bd85
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2019-10-06 18:15:40 +0700

    change depoy script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-claat export lesson1.md
-gulp dist --base-url=https://codelabs.kmp.icerock.dev
+claat export -o codelabs lesson1.md
+gulp dist --base-url=https://codelabs.kmp.icerock.dev --codelabs-dir=codelabs
 rm -r docs
 mv dist docs
