7deb877575c7beed62da9d7dcee843fecc0deadb
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2019-10-06 20:33:27 +0700

    update deploy script

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ claat export -o codelabs lesson1.md
 gulp dist --base-url=https://codelabs.kmp.icerock.dev --codelabs-dir=codelabs
 rm -r docs
 mv dist docs
+rm docs/codelabs
+mv codelabs docs/codelabs
