00cbca2747b955743c5a81d8474036e0064beff2
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2019-10-28 20:12:43 +0700

    replace first lesson to en version

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-claat export -o codelabs lesson1.md
+claat export -o codelabs lesson1-en.md
 claat export -o codelabs giphy-app-1.md
 claat export -o codelabs giphy-app-2.md
 gulp dist
