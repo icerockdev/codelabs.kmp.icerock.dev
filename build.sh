6525ae61a93e79e09539a8c3b74b099445150809
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2019-11-01 14:17:39 +0700

    english version publication

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 claat export -o codelabs lesson1-en.md
-claat export -o codelabs giphy-app-1.md
-claat export -o codelabs giphy-app-2.md
+claat export -o codelabs giphy-app-1-en.md
+claat export -o codelabs giphy-app-2-en.md
 gulp dist
 rm -r docs
 mv dist docs
