765b3e360478c2b321ffd82872d15bfb51751e83
GitHub Action <action@github.com>
2021-05-02 16:47:37 +0700

    Update versions

--- publish-to-github-pages.sh
+++ publish-to-github-pages.sh
@@ -4,9 +4,9 @@
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 
-git clone -b gh-pages git@github.com:icerockdev/codelabs.kmp.icerock.dev.git build
 ./build.sh
 ./node_modules/.bin/gulp build
+git clone -b gh-pages git@github.com:icerockdev/codelabs.kmp.icerock.dev.git build
 rm  build/codelabs
 cp -R  codelabs build/codelabs
 cd build
