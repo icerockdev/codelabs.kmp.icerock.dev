5c50021e625c8715104e11429c5ce24307659004
GitHub Action <action@github.com>
2021-05-02 16:46:15 +0700

    Update versions

--- publish-to-github-pages.sh
+++ publish-to-github-pages.sh
@@ -7,7 +7,10 @@ git config --local user.name "GitHub Action"
 git clone -b gh-pages git@github.com:icerockdev/codelabs.kmp.icerock.dev.git build
 ./build.sh
 ./node_modules/.bin/gulp build
+rm  build/codelabs
+cp -R  codelabs build/codelabs
+cd build
 git add .
 git commit -m "Update versions" -a
-remote_repo="https://${GITHUB_ACTOR}:$2}@github.com/${GITHUB_REPOSITORY}.git"
-git push "${remote_repo}" HEAD:gh-pages
+#remote_repo="https://${GITHUB_ACTOR}:$2}@github.com/${GITHUB_REPOSITORY}.git"
+#git push "${remote_repo}" HEAD:gh-pages
