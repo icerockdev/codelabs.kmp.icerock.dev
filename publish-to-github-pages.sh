553e06d4b3690ec4c772763640e9ad02e87d4eab
GitHub Action <action@github.com>
2021-05-02 16:58:56 +0700

    Publish logic fixes

--- publish-to-github-pages.sh
+++ publish-to-github-pages.sh
@@ -4,6 +4,7 @@
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 
+rm -rf build
 git clone -b gh-pages git@github.com:icerockdev/codelabs.kmp.icerock.dev.git build
 ./build.sh
 ./node_modules/.bin/gulp build
@@ -12,5 +13,5 @@ cp -R  codelabs build/codelabs
 cd build
 git add .
 git commit -m "Update versions" -a
-#remote_repo="https://${GITHUB_ACTOR}:$2}@github.com/${GITHUB_REPOSITORY}.git"
-#git push "${remote_repo}" HEAD:gh-pages
+remote_repo="https://${GITHUB_ACTOR}:$2}@github.com/${GITHUB_REPOSITORY}.git"
+git push "${remote_repo}" HEAD:gh-pages
